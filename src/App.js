e1be3f3c97d2377ca1342d025399ed42532aa2d4
Patrick Boutin <patrick.boutin@roberthalf.com>
2023-12-22 20:53:26 -0800

    Set simple form within BookCreate and lifted title state up to App. Implemented initial state in App. Inserted global styles,

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { useState } from 'react';
+
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
 function App() {
 
+  const [books, setBooks] = useState([]);
+
+  const createBook = title => console.log('Need to add book with title:', title);
   return (
     <div>
-      <h1>App</h1>
-      <BookCreate />
+      <BookCreate onCreate={createBook} />
       <BookList />
     </div>
   );
--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,5 +1,26 @@
-function BookCreate() {
-  return <h2>BookCreate</h2>;
+import { useState } from 'react';
+
+function BookCreate({ onCreate }) {
+
+  const [title, setTitle] = useState('');
+
+  const handleChange = e => {
+    setTitle(e.target.value);
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    onCreate(title);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>BookCreate</h2>
+      <label>Title</label>
+      <input onChange={handleChange} value={title} />
+      <button>Create Book</button>
+    </form>
+  );
 }
 
 export default BookCreate;
